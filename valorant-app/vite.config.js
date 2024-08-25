5267bd6e27dd36e2e808e4c021c2cc815a27582e
thiagoAlves0 <thiago.alvessb1@gmail.com>
2024-08-25 11:16:34 -0300

    Adiciona configuração do Vercel e atualiza vite.config.js

--- valorant-app/vite.config.js
+++ valorant-app/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    // Opcional: você pode adicionar configurações do servidor, se necessário
+    port: 3000,
+  }
+});
